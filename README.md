d040bdd3abdc1a6f688b25c69ba92941cffc87a7
Nathan Banks <nathantb3@gmail.com>
2024-09-30 23:13:55 +0100

    refactor: use argparse, add type hints

--- README.md
+++ README.md
@@ -1,75 +1,21 @@
-# Wireguard-AllowedIPs-Calculator
----
+# WireGuard-AllowedIPs-Calculator
+A simple WireGuard AllowedIPs calculator.
 
-# Simple Wireguard AllowedIPs Calculator
+## Usage
 
-## Usage:
+```
+wg.py [-h] -a AllowedIPs -d DisallowedIPs
 
-## wg.py (Cli output)
-
-`python3 wg.py -a 0.0.0.0,2001:db8::/32 -d 2001:db8:4001:103::/64`
-
-[-a] AllowedIPs. comma to split the multiple ips.
-[-d] DisallowedIPs. comma to split the multiple ips.
-
-## wgpy.py
-
-The same version but accept parameter in python script. you can modify it to your code.
-
-```python
-allow_ip_raw = ["0.0.0.0/0","::0/0"]
-# Replace the allowed(routed) IP ranges with your IP.
-disallow_ip_raw = ["10.0.0.0/8","fc00::/7"]
-# Replace the disallowed(not routed) IP ranges with your IP.
+options:
+  -h, --help        show this help message and exit
+  -a AllowedIPs     allowed CIDR networks (comma-separated)
+  -d DisallowedIPs  disallowed CIDR networks (comma-separated)
 ```
 
-Warning:THIS PROGRAM IS NO GUARANTEE! PLEASE USE IT AT YOUR OWN RISK. ILLEGAL USE IS STRICTLY PROHIBITED.
+For example:
 
----
-# 简单好用的Wireguard路由IP计算器
-
-## 使用方法:
-
-## wg.py (Cli输出)
-
-`python3 wg.py -a 0.0.0.0,2001:db8::/32 -d 2001:db8:4001:103::/64`
-
-[-a] 允许的IP段，使用逗号分割。
-[-d] 不允许的IP段，使用逗号分割。
-
-## wgpy.py
-
-相同版本，但不通过Cli指定参数，可以修改后应用于其他代码。
-
-```python
-allow_ip_raw = ["0.0.0.0/0","::0/0"]
-# 用允许的IP(路由的)IP段来替换上面的内容。
-disallow_ip_raw = ["10.0.0.0/8","fc00::/7"]
-# 用不允许的IP(直接路由不通过wg的IP段)IP段来替换上面的内容。
 ```
+$ python wg.py -a 0.0.0.0,2001:db8::/32 -d 2001:db8:4001:103::/64
 
-警告：本程序无任何保证, 请自行承担使用风险。严禁利用本程序进行非法活动。本程序作者不负任何责任。
-
-# 簡単なWireguard AllowedIPs計算機
-
-## 使用方法:
-
-## wg.py (Cli出力)
-
-`python3 wg.py -a 0.0.0.0,2001:db8::/32 -d 2001:db8:4001:103::/64`
-
-[-a] アクセス許可のIP範囲指定，コンマで多IPを分割する。
-[-d] アクセス禁止のIP範囲指定，コンマで多IPを分割する。
-
-## wgpy.py
-
-上記と同じだが、Cliでパラメータを取得しない。プログラム内で指定する。編集した後で他のコードでも利用可能。
-
-```python
-allow_ip_raw = ["0.0.0.0/0","::0/0"]
-# アクセス許可のIP(ルートしたいIP)で替える。
-disallow_ip_raw = ["10.0.0.0/8","fc00::/7"]
-# アクセス禁止のIP(ルートしたくないIP)で替える。
+AllowedIPs = 0.0.0.0/32, 2001:db8:8000::/33, 2001:db8::/34, 2001:db8:6000::/35, 2001:db8:5000::/36, 2001:db8:4800::/37, 2001:db8:4400::/38, 2001:db8:4200::/39, 2001:db8:4100::/40, 2001:db8:4080::/41, 2001:db8:4040::/42, 2001:db8:4020::/43, 2001:db8:4010::/44, 2001:db8:4008::/45, 2001:db8:4004::/46, 2001:db8:4002::/47, 2001:db8:4000::/48, 2001:db8:4001:8000::/49, 2001:db8:4001:4000::/50, 2001:db8:4001:2000::/51, 2001:db8:4001:1000::/52, 2001:db8:4001:800::/53, 2001:db8:4001:400::/54, 2001:db8:4001:200::/55, 2001:db8:4001::/56, 2001:db8:4001:180::/57, 2001:db8:4001:140::/58, 2001:db8:4001:120::/59, 2001:db8:4001:110::/60, 2001:db8:4001:108::/61, 2001:db8:4001:104::/62, 2001:db8:4001:100::/63, 2001:db8:4001:102::/64
 ```
-
-ご注意ください：本プロジェクトは保証なし。自己責任。本プロジェクトを利用して違法犯罪行為をすることは固く禁じます。
--- wg.py
+++ wg.py
@@ -1,21 +1,29 @@
-from ipaddress import *
-import sys
+import argparse
+import ipaddress
+import typing
 
-def check_ip_network_type(address):
+
+T = typing.TypeVar("T", list[ipaddress.IPv4Network], list[ipaddress.IPv6Network])
+
+
+def check_ip_network_type(address: str):
     try:
-        return 4 if type(ip_network(address)) is IPv4Network else 6
+        return 4 if type(ipaddress.ip_network(address)) is ipaddress.IPv4Network else 6
     except ValueError:
         return 10
 
 
-def calc_wg_ip_range(allowed_networking: list, disallowed_networking: list) -> list:
+def calc_wg_ip_range(
+    allowed_networking: T,
+    disallowed_networking: T,
+) -> list:
     addr = []
-    output = []
+    output: list[str] = []
     for i in allowed_networking:
         count = 0
         for j in disallowed_networking:
-            if j.subnet_of(i):
-                for allowedip in i.address_exclude(j):
+            if j.subnet_of(i):  # type: ignore
+                for allowedip in i.address_exclude(j):  # type: ignore
                     addr.append(allowedip)
                 count += 1
         if count == 0:
@@ -29,27 +37,55 @@ def calc_wg_ip_range(allowed_networking: list, disallowed_networking: list) -> l
     return output
 
 
-data = sys.argv[1:]
-if len(data) == 0:
-    print("Usage: [-a] AllowedIPs. comma to split the multiple ips. [-d] DisallowedIPs. comma to split\nExample: allow 0.0.0.0/0, 2001:db8::/32 and disallow 2001:db8:4001:103::/64, please use python3 wg.py -a 0.0.0.0,2001:db8::/32 -d 2001:db8:4001:103::/64")
-    exit(1)
-allow_ip_raw = data[data.index('-a') + 1].split(',')
-disallow_ip_raw = data[data.index('-d') + 1].split(',')
-allowed_ipv4 = []
-allowed_ipv6 = []
-disallowed_ipv4 = []
-disallowed_ipv6 = []
-for ipa in allow_ip_raw:
-    if check_ip_network_type(ipa) == 4:
-        allowed_ipv4.append(IPv4Network(ipa))
-    elif check_ip_network_type(ipa) == 6:
-        allowed_ipv6.append(IPv6Network(ipa))
-for ipd in disallow_ip_raw:
-    if check_ip_network_type(ipd) == 4:
-        disallowed_ipv4.append(IPv4Network(ipd))
-    elif check_ip_network_type(ipd) == 6:
-        disallowed_ipv6.append(IPv6Network(ipd))
-result = calc_wg_ip_range(allowed_ipv4, disallowed_ipv4) + calc_wg_ip_range(allowed_ipv6, disallowed_ipv6)
-output = list(set(result))
-output.sort(key=result.index)
-print(f"AllowedIPs = {', '.join(output)}")
+def check_raw_ips(allow_ip_raw, disallow_ip_raw):
+    allowed_ipv4: list[ipaddress.IPv4Network] = []
+    allowed_ipv6: list[ipaddress.IPv6Network] = []
+    disallowed_ipv4: list[ipaddress.IPv4Network] = []
+    disallowed_ipv6: list[ipaddress.IPv6Network] = []
+    for ipa in allow_ip_raw:
+        if check_ip_network_type(ipa) == 4:
+            allowed_ipv4.append(ipaddress.IPv4Network(ipa))
+        elif check_ip_network_type(ipa) == 6:
+            allowed_ipv6.append(ipaddress.IPv6Network(ipa))
+    for ipd in disallow_ip_raw:
+        if check_ip_network_type(ipd) == 4:
+            disallowed_ipv4.append(ipaddress.IPv4Network(ipd))
+        elif check_ip_network_type(ipd) == 6:
+            disallowed_ipv6.append(ipaddress.IPv6Network(ipd))
+    return allowed_ipv4, allowed_ipv6, disallowed_ipv4, disallowed_ipv6
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-a",
+        required=True,
+        type=str,
+        metavar="AllowedIPs",
+        help="allowed CIDR networks (comma-separated)",
+    )
+    parser.add_argument(
+        "-d",
+        required=True,
+        type=str,
+        metavar="DisallowedIPs",
+        help="disallowed CIDR networks (comma-separated)",
+    )
+
+    args = parser.parse_args()
+
+    allow_ip_raw: list[str] = args.a.split(",")
+    disallow_ip_raw: list[str] = args.d.split(",")
+    allowed_ipv4, allowed_ipv6, disallowed_ipv4, disallowed_ipv6 = check_raw_ips(
+        allow_ip_raw, disallow_ip_raw
+    )
+    result = calc_wg_ip_range(allowed_ipv4, disallowed_ipv4) + calc_wg_ip_range(
+        allowed_ipv6, disallowed_ipv6
+    )
+    output = list(set(result))
+    output.sort(key=result.index)
+    print(f"AllowedIPs = {', '.join(output)}")
+
+
+if __name__ == "__main__":
+    main()
